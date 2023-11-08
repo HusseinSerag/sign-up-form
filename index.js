387bed70663920f710450b87497d958b00afc383
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-11-08 21:18:38 +0200

    commit

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const confirmPassword = document.getElementById('confirm-password')
 
 
 confirmPassword.addEventListener('input',()=>{
+        password.classList.remove('right')
+        password.classList.remove('right')
+        password.classList.remove('error')
+        confirmPassword.classList.remove('error')
     if(confirmPassword.value != password.value){
         confirmPassword.classList.add('error')
         password.classList.add('error')
-        password.classList.remove('right')
-        password.classList.remove('right')
+        
     }
     else{
-        confirmPassword.classList.remove('error')
+        
         confirmPassword.classList.add('right')
-        password.classList.remove('error')
+      
         password.classList.add('right')
     }
 })
